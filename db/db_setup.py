1c575bb46549cc02dcc8cb407c8bbbd769b5c28c
waiterorg <iwaiterorg@gmail.com>
2022-06-04 18:55:08 +0430

    Setup app to database - SQLAlchemy ORM

---
+++ db/db_setup.py
@@ -0,0 +1,23 @@
+from sqlalchemy import create_engine
+from sqlalchemy.ext.declarative import declarative_base
+from sqlalchemy.orm import sessionmaker
+
+SQLALCHEMY_DATABASE_URL = "postgresql+psycopg2://postgres:postgres@pgdb/waiter_lms"
+# SQLALCHEMY_DATABASE_URL = "postgresql://user:password@postgresserver/db"
+
+engine = create_engine(
+    SQLALCHEMY_DATABASE_URL, connect_args={}, future=True
+)
+SessionLocal = sessionmaker(
+    autocommit=False, autoflush=False, bind=engine, future=True
+)
+
+Base = declarative_base()
+
+# DB Utilities
+def get_db():
+    db = SessionLocal()
+    try:
+        yield db
+    finally:
+        db.close()
