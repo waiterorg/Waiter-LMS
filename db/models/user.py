028486e46f4f90d81f8ef3f51451b481eca5573c
waiterorg <iwaiterorg@gmail.com>
2022-06-04 19:07:53 +0430

    Create User model

---
+++ db/models/user.py
@@ -0,0 +1,39 @@
+import enum
+
+from sqlalchemy import Boolean, Column, ForeignKey, Integer, String, Enum, Text
+from sqlalchemy.orm import relationship
+from sqlalchemy.sql.expression import null
+
+from ..db_setup import Base
+from .mixins import Timestamp
+
+
+class Role(enum.IntEnum):
+    teacher = 1
+    student = 2
+
+
+class User(Timestamp, Base):
+    __tablename__ = "users"
+
+    id = Column(Integer, primary_key=True, index=True)
+    email = Column(String(100), unique=True, index=True, nullable=False)
+    role = Column(Enum(Role))
+    is_active = Column(Boolean, default=False)
+
+    profile = relationship("Profile", back_populates="owner", uselist=False)
+    student_courses = relationship("StudentCourse", back_populates="student")
+    student_content_blocks = relationship(
+        "CompletedContentBlock", back_populates="student")
+
+
+class Profile(Timestamp, Base):
+    __tablename__ = "profiles"
+
+    id = Column(Integer, primary_key=True, index=True)
+    first_name = Column(String(50), nullable=False)
+    last_name = Column(String(50), nullable=False)
+    bio = Column(Text, nullable=True)
+    user_id = Column(Integer, ForeignKey("users.id"), nullable=False)
+
+    owner = relationship("User", back_populates="profile")
