ba98aba060963f27f666eee5bce4dc6e20e453f8
waiterorg <iwaiterorg@gmail.com>
2022-06-05 22:57:40 +0430

    setup user and course models

--- main.py
+++ main.py
@@ -1,5 +1,11 @@
 from fastapi import FastAPI
+
 from api import users, courses, sections
+from db.db_setup import engine
+from db.models import user, course
+
+user.Base.metadata.create_all(bind=engine)
+course.Base.metadata.create_all(bind=engine)
 
 app = FastAPI(
     title="WAITER LMS",
