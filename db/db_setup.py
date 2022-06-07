814a8b81caa89bcb45bfb65ceb74194c25f3ea35
waiterorg <iwaiterorg@gmail.com>
2022-06-07 17:09:12 +0430

    setup db async

--- db/db_setup.py
+++ db/db_setup.py
@@ -1,15 +1,21 @@
 from sqlalchemy import create_engine
+from sqlalchemy.ext.asyncio import AsyncSession, create_async_engine
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import sessionmaker
 
-SQLALCHEMY_DATABASE_URL = "postgresql+psycopg2://postgres:postgres@localhost/waiter_lms"
-# SQLALCHEMY_DATABASE_URL = "postgresql://user:password@postgresserver/db"
-
-engine = create_engine(
-    SQLALCHEMY_DATABASE_URL, connect_args={}, future=True
+SQLALCHEMY_DATABASE_URL = (
+    "postgresql+psycopg2://postgres:postgres@localhost/waiter_lms"
 )
+ASYNC_SQLALCHEMY_DATABASE_URL = "postgresql+asyncpg://gwen@localhost/fast_lms"
+
+engine = create_engine(SQLALCHEMY_DATABASE_URL, connect_args={}, future=True)
+async_engine = create_async_engine(ASYNC_SQLALCHEMY_DATABASE_URL)
+
 SessionLocal = sessionmaker(
     autocommit=False, autoflush=False, bind=engine, future=True
+)
+AsyncSessionLocal = sessionmaker(
+    async_engine, class_=AsyncSession, expire_on_commit=False
 )
 
 Base = declarative_base()
@@ -21,3 +27,9 @@ def get_db():
         yield db
     finally:
         db.close()
+
+
+async def async_get_db():
+    async with AsyncSessionLocal() as db:
+        yield db
+        await db.commit()
