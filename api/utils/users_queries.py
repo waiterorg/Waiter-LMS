04419ba26a873348337ca3a8c59a2ba72a773ed1
waiterorg <iwaiterorg@gmail.com>
2022-06-07 17:21:43 +0430

    change get user query to async

--- api/utils/users_queries.py
+++ api/utils/users_queries.py
@@ -1,11 +1,14 @@
-from sqlalchemy.orm import Session
-
 from db.models.user import User
 from pydantic_schemas.user import UserCreate
+from sqlalchemy.ext.asyncio import AsyncSession
+from sqlalchemy.future import select
+from sqlalchemy.orm import Session
 
 
-def get_user(db: Session, user_id: int):
-    return db.query(User).filter(User.id == user_id).first()
+async def get_user(db: AsyncSession, user_id: int):
+    query = select(User).where(User.id == user_id)
+    result = await db.execute(query)
+    return result.scalar_one_or_none()
 
 
 def get_user_by_email(db: Session, email: str):
