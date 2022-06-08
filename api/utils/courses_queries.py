3c48a5415bef9eae6c51cd3fae77c40af3af7586
waiterorg <iwaiterorg@gmail.com>
2022-06-08 18:21:21 +0430

    Add update and delete course query

--- api/utils/courses_queries.py
+++ api/utils/courses_queries.py
@@ -1,4 +1,5 @@
 from db.models.course import Course
+from fastapi import HTTPException, status
 from pydantic_schemas.course import CourseCreate
 from sqlalchemy.orm import Session
 
@@ -25,4 +26,32 @@ def create_course(db: Session, course: CourseCreate):
     db.add(db_course)
     db.commit()
     db.refresh(db_course)
+    return db_course
+
+
+def update_course(db: Session, course_id: int, course: CourseCreate):
+    db_course = get_course(db, course_id)
+    if not db_course:
+        raise HTTPException(
+            status.HTTP_404_NOT_FOUND, detail="Course not found"
+        )
+    update_data = course.dict(exclude_unset=True)
+
+    for key, value in update_data.items():
+        setattr(db_course, key, value)
+
+    db.add(db_course)
+    db.commit()
+    db.refresh(db_course)
+    return db_course
+
+
+def delete_course(db: Session, course_id: int):
+    db_course = get_course(db=db, course_id=course_id)
+    if db_course is None:
+        raise HTTPException(
+            status.HTTP_404_NOT_FOUND, detail="Course not found"
+        )
+    db.delete(db_course)
+    db.commit()
     return db_course
