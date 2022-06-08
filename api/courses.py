790937e40cdafbc6ca1e97ab35e417f3341e0bd1
waiterorg <iwaiterorg@gmail.com>
2022-06-08 18:22:04 +0430

    Create update and delete course api

--- api/courses.py
+++ api/courses.py
@@ -3,10 +3,17 @@ from typing import List
 import fastapi
 from db.db_setup import get_db
 from fastapi import Depends, HTTPException
+from fastapi.encoders import jsonable_encoder
 from pydantic_schemas.course import Course, CourseCreate
 from sqlalchemy.orm import Session
 
-from api.utils.courses_queries import create_course, get_course, get_courses
+from api.utils.courses_queries import (
+    create_course,
+    delete_course,
+    get_course,
+    get_courses,
+    update_course,
+)
 
 router = fastapi.APIRouter()
 
@@ -32,14 +39,16 @@ async def read_course(course_id: int, db: Session = Depends(get_db)):
     return db_course
 
 
-@router.patch("/courses/{id}")
-async def update_course():
-    return {"courses": []}
+@router.patch("/courses/{course_id}", response_model=Course)
+async def update_specified_course(
+    course_id: int, course: CourseCreate, db: Session = Depends(get_db)
+):
+    return update_course(db=db, course_id=course_id, course=course)
 
 
-@router.delete("/courses/{id}")
-async def delete_course():
-    return {"courses": []}
+@router.delete("/courses/{course_id}", status_code=204)
+async def remove_course(course_id: int, db: Session = Depends(get_db)):
+    return delete_course(db=db, course_id=course_id)
 
 
 @router.get("/courses/{id}/sections")
